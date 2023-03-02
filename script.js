3457d8befce21e8c6fdd222fdee55c64c1d36e86
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-03-02 09:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices = document.querySelectorAll('.price');
+	let sum = 0;
+	for (let i = 0; i < prices.length; i++) {
+		let x = Number(prices[i].innerText);
+		sum += x;
+	}
+    document.getElementById('table').innerHTML += 
+		`
+	   <tr id="ans">
+          <td></td>
+          <td>${sum}</td>
+        </tr>`
   
 };
 
